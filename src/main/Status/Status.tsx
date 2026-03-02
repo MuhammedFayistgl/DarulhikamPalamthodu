830ed9c5bf7720e1a6882b229fbc2bf3f8d05599
fayis <muhammedfayisthangal@gmail.com>
2026-03-02 20:30:26 +0530

    add name

--- src/main/Status/Status.tsx
+++ src/main/Status/Status.tsx
@@ -1,4 +1,4 @@
-import { Card, Image } from "antd";
+import { Card, Image, Typography } from "antd";
 import type { FC } from "react";
 
 interface CertificateProps { }
@@ -18,7 +18,10 @@ const Certificate: FC<CertificateProps> = () => {
 
                 style={{ transition: 'all 0.3s ease', marginTop: '30%' }}
                 width={"100%"}
-                src="https://i.ibb.co/SDqckczs/a.jpg"></Image>
+                src="https://img.sanishtech.com/u/d4739b731e91401442a9ca2a1ab1e079.jpg">
+
+                    {/* <Typography>hai</Typography> */}
+                </Image>
         </Card >
 
 
